d20f7b84c6bf3f6bf3a63ee36ae22a3a85156ee4
Gwendolyn Holley <gmfholley@gmail.com>
2015-10-27 14:41:31 -0500

    Creates basic express routing app and changes output to json on index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify({a: 1}));
+//  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
