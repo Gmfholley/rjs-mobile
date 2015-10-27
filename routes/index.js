7b7340fa51cfef11d6062f77087ef8e128cba2fb
Gwendolyn Holley <gmfholley@gmail.com>
2015-10-27 16:41:12 -0500

    Attempts to chain together three middleware steps in the request and require cheerio and xmlhttprequest

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+var cheerio = require('cheerio');
+var xmlHttp = require('xmlhttprequest').XMLHttpRequest;
+ //   $ = cheerio.load('<h2 class="title">Hello world</h2>');
 
+//$('h2.title').text('Hello there!');
+//$('h2').addClass('welcome');
+
+//$.html();
 /* GET home page. */
-router.get('/', function(req, res) {
-//	res.setHeader('Content-Type', 'application/html');
+//router.get('/', function(req, res, next) {
+	//res.setHeader('Content-Type', 'application/html');
 	//res.send(JSON.stringify({a: 1}));
     //res.render('index', { title: 'Express' });
-	res.sendFile(path.join(__dirname + '/../views/plain.html'));
+
+    next();
+}, function(req, res, next){
+
+	next();
+}, function(req, res, next){
+	res.sendFile(path.join(__dirname + '/../views/plain.html'));	
 });
 
 module.exports = router;
