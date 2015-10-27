6ad38438506035d455df186de9dfd2f49dbf4cb8
Gwendolyn Holley <gmfholley@gmail.com>
2015-10-27 16:56:59 -0500

    Now that I've debugged it can be moved back into the middlware piece

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ var router = express.Router();
 var cheerio = require('cheerio');
 var xmlHttp = require('xmlhttprequest').XMLHttpRequest;
  //   $ = cheerio.load('<h2 class="title">Hello world</h2>');
-    var xhr = new xmlHttp();
-    xhr.open('get', 'http://as400.farmers-national.com:1080/IMAGESERVER/DOC104R?action=Search', true);
 
 //$('h2.title').text('Hello there!');
 //$('h2').addClass('welcome');
@@ -18,6 +16,8 @@ router.get('/', function(req, res, next) {
     //res.render('index', { title: 'Express' });
    //xhr.send();
    // console.log('Time');
+    var xhr = new xmlHttp();
+    xhr.open('get', 'http://as400.farmers-national.com:1080/IMAGESERVER/DOC104R?action=Search', true);
     next();
 }, function(req, res, next){
 
