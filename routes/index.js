0f65a09bc966653903df210d67d32d2b1e6db61b
Gwendolyn Holley <gmfholley@gmail.com>
2015-10-27 16:06:57 -0500

    Finally have it serving up html instead of the contents of the file.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify({a: 1}));
-//  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+//	res.setHeader('Content-Type', 'application/html');
+	//res.send(JSON.stringify({a: 1}));
+    //res.render('index', { title: 'Express' });
+	res.sendFile(path.join(__dirname + '/../views/plain.html'));
 });
 
 module.exports = router;
