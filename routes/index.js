e4945470e578b1ba5077e9b2c93c7d66b1c48025
Gwendolyn Holley <gmfholley@gmail.com>
2015-10-27 16:56:17 -0500

    Properly  initializes xmlhttprequest and moves it outside the middleware

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var cheerio = require('cheerio');
 var xmlHttp = require('xmlhttprequest').XMLHttpRequest;
  //   $ = cheerio.load('<h2 class="title">Hello world</h2>');
+    var xhr = new xmlHttp();
+    xhr.open('get', 'http://as400.farmers-national.com:1080/IMAGESERVER/DOC104R?action=Search', true);
 
 //$('h2.title').text('Hello there!');
 //$('h2').addClass('welcome');
@@ -14,9 +16,8 @@ router.get('/', function(req, res, next) {
 	//res.setHeader('Content-Type', 'application/html');
 	//res.send(JSON.stringify({a: 1}));
     //res.render('index', { title: 'Express' });
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', 'http://as400.farmers-national.com:1080/IMAGESERVER/DOC104R?action=Search', true);
-    xhr.send();
+   //xhr.send();
+   // console.log('Time');
     next();
 }, function(req, res, next){
 
